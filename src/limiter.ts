6d66a8ccec2ce7246bf3c9f8fb4f5befb120bc88
berat-552 <berat.d7599@gmail.com>
2024-01-27 20:44:18 +0000

    limit increase

--- src/limiter.ts
+++ src/limiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1h
-  max: 50, // 50 requests/hour
+  max: 400, // 400 requests/hour
   statusCode: 429,
   message: { error: "Too many requests from this IP, please try again later." },
 });
